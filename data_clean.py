933ffd3839adeb2fbf09fd4d17db892ec97f83a2
gary butler <43405097+gary-butler@users.noreply.github.com>
2019-06-18 18:25:58 +0200

    added comment

--- data_clean.py
+++ data_clean.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+#data from https://github.com/several27/FakeNewsCorpus
+
 df = pd.read_csv('news_cleaned_2018_02_13.csv.zip', index_col = 0, compression = 'zip')
 working_df = df.loc[df['tag'].isin(['fake', 'reliable']), ['content', 'type']]
 working_df.to_csv('news_content_type.csv', index=False)
